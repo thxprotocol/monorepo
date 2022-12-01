9a85f8a142952a2c003fcedcc39204ebe9262b6b
Peter <peter@thx.network>
2022-12-01 13:55:41 +0100

    Layout fixes

--- apps/widget/src/App.vue
+++ apps/widget/src/App.vue
@@ -1,14 +1,26 @@
 <template>
     <div class="d-flex flex-column h-100">
-        <b-navbar type="dark" variant="dark" class="navbar-top">
-            <b-button @click="onClick()">x</b-button>
-            <b-button v-if="!accountStore.isAuthenticated" @click="accountStore.api.signin()">Signin</b-button>
-            <b-button v-else @click="accountStore.api.signout()">Signout</b-button>
+        <b-navbar type="dark" variant="dark" class="navbar-top pt-3">
+            <b-button variant="link" @click="onClick()"> <i class="fas fa-times text-white"></i></b-button>
+            <b-button
+                variant="link"
+                v-if="!accountStore.isAuthenticated"
+                @click="accountStore.api.signin()"
+                class="text-white"
+            >
+                Signin <i class="fas fa-sign-in-alt ml-1"></i>
+            </b-button>
+            <b-dropdown variant="link" v-else no-caret right>
+                <template #button-content>
+                    <i class="fas fa-bars text-white"></i>
+                </template>
+                <b-dropdown-item-button @click="accountStore.api.signout()">Signout</b-dropdown-item-button>
+            </b-dropdown>
         </b-navbar>
         <div class="flex-grow-1 overflow-auto">
             <router-view />
         </div>
-        <b-navbar type="dark" variant="dark" class="navbar-bottom">
+        <b-navbar variant="dark" class="navbar-bottom">
             <router-link to="/" class="d-flex flex-column justify-content-center align-items-center">
                 <i class="fas fa-coins"></i>
                 Points
--- apps/widget/src/components/BaseCardRewardReferral.vue
+++ apps/widget/src/components/BaseCardRewardReferral.vue
@@ -4,22 +4,29 @@
             <div>
                 <img
                     class="img-brand"
-                    :src="`https://avatars.dicebear.com/api/identicon/${reward.brand}.svg`"
-                    :alt="String(reward.platform)"
+                    :src="`https://avatars.dicebear.com/api/identicon/${reward.platform}.svg`"
+                    :alt="reward.platform"
                 />
             </div>
             <div class="flex-grow-1">{{ reward.title }}</div>
             <div class="text-success">{{ reward.amount }}</div>
         </b-card-title>
+
         <b-card-text>
             {{ reward.description }}
         </b-card-text>
-        <b-button variant="primary" block class="w-100" :disabled="reward.claimed" @click="onClick">
-            <template v-if="reward.claimed"> Claimed </template>
-            <template v-else>
-                Claim
-                <strong>{{ reward.amount }} points</strong>
-            </template>
+
+        <b-input-group v-if="accountStore.isAuthenticated" size="sm">
+            <b-form-input :model-value="`${baseUrl}${reward._id}`" size="sm" />
+            <b-input-group-append>
+                <b-button size="sm" variant="primary">
+                    <i class="fas fa-clipboard"></i>
+                </b-button>
+            </b-input-group-append>
+        </b-input-group>
+
+        <b-button v-else variant="primary" block class="w-100" @click="accountStore.api.signin()">
+            Claim <strong>{{ reward.amount }} points</strong>
         </b-button>
     </b-card>
 </template>
@@ -27,6 +34,7 @@
 <script lang="ts">
 import { mapStores } from 'pinia';
 import { defineComponent, PropType } from 'vue';
+import { useAccountStore } from '../stores/Account';
 import { useRewardStore } from '../stores/Reward';
 
 export default defineComponent({
@@ -37,12 +45,20 @@ export default defineComponent({
             required: true,
         },
     },
+    data: () => {
+        return { baseUrl: 'https://xyz.com?referral=' };
+    },
     computed: {
+        ...mapStores(useAccountStore),
         ...mapStores(useRewardStore),
     },
     methods: {
-        onClick: () => {
-            // signin
+        onClick: function () {
+            if (this.accountStore.isAuthenticated) {
+                alert('authenticated');
+            } else {
+                alert('not authenticated');
+            }
         },
     },
 });
@@ -61,6 +77,13 @@ export default defineComponent({
 }
 .btn-primary {
     background-color: #5942c1;
+}
+.form-control {
+    border-color: #241956;
+    background-color: #241956;
+    color: white;
+}
+.btn-block {
     border-radius: 25px;
 }
 
