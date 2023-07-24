087d503a9dc0f7e47207107b48a039ee4f20aff2
Peter <peter@thx.network>
2023-07-24 09:19:39 +0200

    Debug info

--- apps/widget/src/views/campaign/CollectWallet.vue
+++ apps/widget/src/views/campaign/CollectWallet.vue
@@ -19,7 +19,10 @@
             <b-form-group description="Provide a wallet code to connect it and complete your quests.">
                 <b-form-input :state="isValidUUID" v-model="uuid" placeholder="Code" />
             </b-form-group>
-
+            {{ !!error }}
+            {{ error }}
+            {{ isWaitingForWalletAddress }}
+            {{ isLoadingCollect }}
             <b-button
                 v-if="authStore.oAuthShare"
                 @click="onClickCollect"
