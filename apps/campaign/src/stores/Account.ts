87ff9df04ec14a9c6eadb88da7e600b675d1caac
Peter <peter@thx.network>
2023-08-12 15:25:04 +0200

    fix daily color

--- apps/campaign/src/stores/Account.ts
+++ apps/campaign/src/stores/Account.ts
@@ -40,13 +40,6 @@ export const useAccountStore = defineStore('account', {
             const data = { ...this.getConfig(poolId), ...config };
             localStorage.setItem(`thx:widget:${poolId}:config`, JSON.stringify(data));
             this.poolId = poolId;
-
-            // thx.network and dev-www.thx.network
-            // if (
-            //     !['6286444ba9838bc5ed9fe117', '62f3cad8f838cc0640563e41', '64d499eb1651f15b76a56a0b'].includes(poolId)
-            // ) {
-            //     window.location.href = WIDGET_URL + '/maintenance';
-            // }
         },
         setTheme(config: TWidgetConfig) {
             const { title, theme } = config;
