35c8da051feedf2a25aa8eca0d67817977cf289d
Peter <peter@thx.network>
2023-11-21 16:01:24 +0100

    Increase campaign limits

--- apps/campaign/src/views/discovery/Home.vue
+++ apps/campaign/src/views/discovery/Home.vue
@@ -156,7 +156,7 @@ export default defineComponent({
             imgLogo,
             isLoading: true,
             page: 1,
-            limit: 8,
+            limit: 15,
             search: '',
             debouncedSearch: null,
             screenWidth: window.innerWidth,
