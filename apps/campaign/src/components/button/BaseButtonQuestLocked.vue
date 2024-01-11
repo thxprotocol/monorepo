ca1c84ce45e342943b20d86878f2b7fda4c44180
Peter <peter@thx.network>
2024-01-11 09:58:19 +0100

    Add quest lock modal

--- apps/campaign/src/components/button/BaseButtonQuestLocked.vue
+++ apps/campaign/src/components/button/BaseButtonQuestLocked.vue
@@ -1,11 +1,25 @@
 <template>
-    <b-button variant="primary" block class="w-100" disabled>
+    <b-button variant="primary" block class="w-100" @click="isModalShown = true">
         <i class="fas fa-lock me-1" />
         Quest Locked
     </b-button>
+    <b-modal title="Quest is locked!" :id="modalId" v-model="isModalShown" centered no-close-on-backdrop>
+        <p class="text-opaque">To unlock this quest, complete these quests:</p>
+        <template v-for="lock of locks">
+            <div class="d-flex justify-content-between">
+                {{ lock.title }}
+                <strong class="text-accent">{{ lock.pointsAvailable }}</strong>
+            </div>
+        </template>
+        <template #footer>
+            <b-button class="w-100" variant="primary" @click="isModalShown = false">Continue</b-button>
+        </template>
+    </b-modal>
 </template>
 
 <script lang="ts">
+import { mapStores } from 'pinia';
+import { useRewardStore } from '../../stores/Reward';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
@@ -14,6 +28,21 @@ export default defineComponent({
         quest: {
             type: Object,
             required: true,
+        },
+    },
+    data() {
+        return { isModalShown: false };
+    },
+    computed: {
+        ...mapStores(useRewardStore),
+        modalId() {
+            return `modalQuestLock${this.quest._id}`;
+        },
+        locks() {
+            if (!this.rewardsStore.quests.length) return;
+            return this.quest.locks.map((lock: { questId: string }) =>
+                this.rewardsStore.quests.find((q) => lock.questId === q._id),
+            );
         },
     },
     mounted() {
