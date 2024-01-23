aa1802106c4e1d2caf3036cf0a9638d9274f2014
Peter <peter@thx.network>
2024-01-23 01:21:49 +0100

    Adds notify

---
+++ apps/campaign/public/notify.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+
+require('dotenv').config();
+
+const args = process.argv.slice(2);
+const [branch, version, webhook] = args;
+
+if (!branch || !version || !webhook) {
+    console.error('Usage: yarn notify :app :version :webhook');
+    process.exit(1);
+}
+const app = branch === 'main' ? 'AppProd' : 'AppDev';
+const message = `✅ Released ${app} \`${version}\``;
+
+async function sendNotification() {
+    try {
+        await axios.post(webhook, {
+            content: message,
+        });
+        console.log('Discord Message sent successfully');
+    } catch (error) {
+        console.error('Error sending notification:', error.message);
+    }
+}
+
+if (webhook) {
+    sendNotification();
+}
