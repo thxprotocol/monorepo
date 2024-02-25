c5fa5c53af83ac8bbbbe3bfadad7d63394e09a68
Peter <peter@thx.network>
2024-02-25 14:55:33 +0100

    Show message in wallet view for anon users

--- apps/campaign/src/views/Wallets.vue
+++ apps/campaign/src/views/Wallets.vue
@@ -1,6 +1,10 @@
 <template>
     <b-container>
-        <b-row>
+        <b-alert v-if="isAlertShown" variant="primary" v-model="isAlertShown" class="py-2 mt-3">
+            <i class="fas fa-gift me-2" />
+            Sign in to see your rewards!
+        </b-alert>
+        <b-row v-else>
             <b-col lg="6" offset-lg="3" class="px-0">
                 <BaseCardWallets />
                 <BaseCardRewards />
@@ -11,8 +15,16 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { useAccountStore } from '../stores/Account';
+import { mapStores } from 'pinia';
 
 export default defineComponent({
     name: 'BaseViewWallets',
+    computed: {
+        ...mapStores(useAccountStore),
+        isAlertShown() {
+            return !this.accountStore.isAuthenticated;
+        },
+    },
 });
 </script>
