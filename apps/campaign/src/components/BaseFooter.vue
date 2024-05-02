e5708baeb35e4488742c7fba36d7ced825e23de4
Peter <peter@thx.network>
2024-05-02 10:24:56 +0200

    Fix footer alignment

--- apps/campaign/src/components/BaseFooter.vue
+++ apps/campaign/src/components/BaseFooter.vue
@@ -1,7 +1,7 @@
 <template>
-    <b-container class="pt-5 pb-3">
+    <b-container class="py-5">
         <b-row>
-            <b-col md="3">
+            <b-col md="4">
                 <div class="text-opaque my-3">THX Network</div>
                 <b-nav>
                     <b-nav-item href="https://thx.network" target="_blank" link-class="px-0" class="w-100">
@@ -13,11 +13,29 @@
                     <b-nav-item href="https://dashboard.thx.network" target="_blank" link-class="px-0" class="w-100">
                         Dashboard
                     </b-nav-item>
+                    <b-nav-item
+                        href="https://thx.network/privacy-policy.pdf"
+                        target="_blank"
+                        class="w-100"
+                        link-class="text-white px-0"
+                    >
+                        Privacy Policy
+                    </b-nav-item>
+                    <b-nav-item
+                        href="https://thx.network/general-terms-and-conditions.pdf"
+                        target="_blank"
+                        class="w-100"
+                        link-class="text-white px-0"
+                    >
+                        Terms of use
+                    </b-nav-item>
                 </b-nav>
             </b-col>
-            <b-col md="3">
+            <b-col md="4">
                 <div class="text-opaque my-3">Campaign Managers</div>
                 <b-nav>
+                    <b-nav-item to="/" link-class="px-0" class="w-100"> Explore Campaigns </b-nav-item>
+                    <b-nav-item to="/earn" link-class="px-0" class="w-100"> Quest & Earn </b-nav-item>
                     <b-nav-item href="https://docs.thx.network" target="_blank" link-class="px-0" class="w-100">
                         Documentation
                     </b-nav-item>
@@ -39,22 +57,7 @@
                     </b-nav-item>
                 </b-nav>
             </b-col>
-            <b-col md="3">
-                <div class="text-opaque my-3">Campaign Participants</div>
-                <b-nav>
-                    <b-nav-item to="/" link-class="px-0" class="w-100"> Explore Campaigns </b-nav-item>
-                    <b-nav-item to="/earn" link-class="px-0" class="w-100"> Quest & Earn </b-nav-item>
-                    <b-nav-item
-                        href="https://discord.com/invite/TzbbSmkE7Y"
-                        target="_blank"
-                        link-class="px-0"
-                        class="w-100"
-                    >
-                        Support
-                    </b-nav-item>
-                </b-nav>
-            </b-col>
-            <b-col md="3">
+            <b-col md="4">
                 <div class="text-opaque my-3">Community</div>
                 <b-nav>
                     <b-nav-item
@@ -76,26 +79,25 @@
                     >
                         YouTube <i class="fas fa-external-link-alt ms-1 small" />
                     </b-nav-item>
+                    <b-nav-item
+                        href="https://discord.com/invite/TzbbSmkE7Y"
+                        target="_blank"
+                        link-class="px-0"
+                        class="w-100"
+                    >
+                        Support
+                    </b-nav-item>
                 </b-nav>
             </b-col>
             <b-col md="12">
-                <b-nav class="my-3">
-                    <b-nav-item href="https://thx.network" target="_blank" link-class="text-white text-opaque px-0">
+                <b-nav class="pt-5 text-center">
+                    <b-nav-item
+                        href="https://thx.network"
+                        target="_blank"
+                        class="w-100"
+                        link-class="text-white text-opaque px-0"
+                    >
                         &copy; 2024 THX Network
-                    </b-nav-item>
-                    <b-nav-item
-                        href="https://thx.network/privacy-policy.pdf"
-                        target="_blank"
-                        link-class="text-white text-opaque"
-                    >
-                        Privacy Policy
-                    </b-nav-item>
-                    <b-nav-item
-                        href="https://thx.network/general-terms-and-conditions.pdf"
-                        target="_blank"
-                        link-class="text-white text-opaque"
-                    >
-                        Terms of use
                     </b-nav-item>
                 </b-nav>
             </b-col>
