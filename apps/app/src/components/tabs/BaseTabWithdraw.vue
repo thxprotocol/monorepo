fd1897ff72dbda4b8109b169b91d0c6735e859b8
Peter <peter@thx.network>
2024-05-21 20:02:02 +0200

    Fix membership card

--- apps/app/src/components/tabs/BaseTabWithdraw.vue
+++ apps/app/src/components/tabs/BaseTabWithdraw.vue
@@ -39,7 +39,23 @@
             </b-form-group>
         </b-col>
         <b-col class="d-flex align-items-center justify-content-center flex-column">
-            <BaseCardMembership />
+            <div class="text-center">
+                <div
+                    class="position-relative rounded-circle m-auto gradient-border-xl"
+                    style="width: 75px; height: 75px"
+                >
+                    <div
+                        class="position-relative bg-dark rounded-circle d-flex align-items-center justify-content-center"
+                        style="z-index: 1; width: 65px; height: 65px"
+                    >
+                        <i class="fas fa-id-card" style="font-size: 1.5rem" />
+                    </div>
+                </div>
+                <b-badge class="mt-2 p-2" variant="primary">
+                    <i class="fas fa-trophy me-1 text-opaque" />
+                    {{ membership }}
+                </b-badge>
+            </div>
         </b-col>
     </b-row>
     <hr />
@@ -93,6 +109,19 @@ export default defineComponent({
         amount() {
             if (!this.veStore.lock) return;
             return fromWei(String(this.veStore.lock.amount));
+        },
+        membership() {
+            if (!this.veStore.lock) return;
+            const price = this.liquidityStore.pricing['20USDC-80THX'];
+            const amount = Number(fromWei(String(this.veStore.lock.amount)));
+            const amountInUSD = amount * price;
+
+            if (amountInUSD < 5) return 'No Rank';
+            if (amountInUSD > 5 && amountInUSD < 50) return 'Rookie';
+            if (amountInUSD > 50 && amountInUSD < 500) return 'Pro';
+            if (amountInUSD > 500 && amountInUSD < 5000) return 'Elite';
+            if (amountInUSD > 5000 && amountInUSD < 50000) return 'Master';
+            if (amountInUSD > 50000 && amountInUSD < 500000) return 'Legend';
         },
     },
     methods: {
