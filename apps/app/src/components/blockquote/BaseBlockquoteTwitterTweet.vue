3f3656cc25105e1064325476ba635a5d7fe18767
Peter <peter@thx.network>
2024-05-24 09:37:02 +0200

    Fix post preview in X cards

--- apps/app/src/components/blockquote/BaseBlockquoteTwitterTweet.vue
+++ apps/app/src/components/blockquote/BaseBlockquoteTwitterTweet.vue
@@ -1,23 +1,19 @@
 <template>
-    <blockquote>
-        <b-link
-            target="_blank"
-            class="fw-bold text-accent"
-            :href="`https://www.twitter.com/${quest.contentMetadata.username}`"
-        >
-            @{{ quest.contentMetadata.username }}
+    <blockquote v-if="post">
+        <b-link target="_blank" class="fw-bold text-accent" :href="`https://www.twitter.com/${post.username}`">
+            @{{ post.username }}
         </b-link>
         -
-        <span v-if="quest.contentMetadata.text"> {{ quest.contentMetadata.text.substring(0, 255) }}</span>
-        <b-link
-            v-if="quest.contentMetadata.text.length > 255"
-            :href="quest.contentMetadata.url"
-            target="_blank"
-            class="text-accent"
-        >
-            ...
-        </b-link>
+        <span v-if="post.text">
+            {{ post.text.substring(0, 255)
+            }}<template v-if="post.text.length > 255" class="text-accent"> ... </template>
+        </span>
+        <b-link v-if="url" :href="url" target="_blank" class="text-accent"> View post </b-link>
     </blockquote>
+    <b-alert v-else variant="primary" class="p-2">
+        <i class="fas fa-info-circle me-1"></i>
+        Post information not available
+    </b-alert>
 </template>
 
 <script lang="ts">
@@ -45,9 +41,12 @@ export default defineComponent({
     computed: {
         ...mapStores(useAccountStore),
         ...mapStores(useQuestStore),
-        content() {
-            if (!this.interactionLabel[this.quest.interaction] || !this.quest.content) return;
-            return this.getChannelActionURL(this.quest.interaction, this.quest.content);
+        post() {
+            return this.quest.contentMetadata;
+        },
+        url() {
+            if (!this.interactionLabel[this.quest.interaction] || !this.quest.content) return '';
+            return this.post.url || this.getChannelActionURL(this.quest.interaction, this.quest.content);
         },
     },
     methods: {
@@ -56,7 +55,7 @@ export default defineComponent({
                 case QuestSocialRequirement.TwitterLike:
                 case QuestSocialRequirement.TwitterRetweet:
                 case QuestSocialRequirement.TwitterLikeRetweet:
-                    return { url: `https://www.twitter.com/twitter/status/${content}` };
+                    return `https://www.x.com/twitter/status/${content}`;
                 default:
                     return '';
             }
