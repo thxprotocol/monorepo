c190012d42d4589cf778c860358a5d81474c3c3c
Peter <peter@thx.network>
2024-06-26 14:26:30 +0200

    Force commit

--- apps/api/scripts/script.ts
+++ apps/api/scripts/script.ts
@@ -9,8 +9,8 @@ import db from '@thxnetwork/api/util/database';
 // import main from './src/safe';
 // import main from './src/ipfs';
 // import main from './src/invoices';
-import main from './src/demo';
-// import main from './src/preview';
+// import main from './src/demo';
+import main from './src/preview';
 // import main from './src/metamask';
 // import main from './src/lottery';
 
