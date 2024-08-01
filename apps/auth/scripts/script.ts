a2000cb983d6d336716b80ca013545281d962b32
Peter <peter@thx.network>
2024-08-01 13:20:58 +0200

    Update migration script

--- apps/auth/scripts/script.ts
+++ apps/auth/scripts/script.ts
@@ -3,7 +3,7 @@ import { Token } from '@thxnetwork/auth/models/Token';
 import { AccountVariant } from '@thxnetwork/common/enums';
 import db from '@thxnetwork/auth/util/database';
 
-db.connect(process.env.MONGODB_URI_DEV);
+db.connect(process.env.MONGODB_URI_PROD);
 
 const accountVariantProviderKindMap: { [variant: number]: string } = {
     [AccountVariant.SSOGoogle]: 'google',
@@ -14,33 +14,42 @@ const accountVariantProviderKindMap: { [variant: number]: string } = {
 };
 
 async function main() {
-    const accounts = await Account.find({});
-    const tokens = await Token.find({ sub: accounts.map((a) => a.id) });
-
-    // Create opeartions for accounts where providerUserId is set based on a.variant
-    const operations = accounts.map((a) => {
-        const kind = accountVariantProviderKindMap[a.variant];
-        const token = tokens.find((t) => t.sub === a.id && t.kind === kind);
-        // console.log('Account', a.id, 'Token', token && token.userId);
-        return {
-            updateOne: {
-                filter: { _id: a.id },
-                update: {
-                    $set: {
-                        // Set account.providerUserId to token.userId
-                        providerUserId: token && token.userId,
-                    },
-                },
-            },
-        };
+    const accounts = await Account.find({
+        providerUserId: { $exists: false },
+        createdAt: { $gte: new Date('2024-08-01') },
     });
+    const tokens = await Token.find({ sub: accounts.map((a) => a.id) });
 
     // Bulk write operations to update accounts in chunks of 1000
     const chunkSize = 1000;
-    for (let i = 0; i < operations.length; i += chunkSize) {
-        const chunk = operations.slice(i, i + chunkSize);
-        console.log('Start chunk', i, chunk.length);
-        await Account.bulkWrite(chunk);
+    for (let i = 0; i < accounts.length; i += chunkSize) {
+        const chunk = accounts.slice(i, i + chunkSize);
+        console.log('Preparing chunk:', i);
+
+        // Create opeartions for accounts where providerUserId is set based on a.variant
+        const operations = chunk
+            .map((a) => {
+                const kind = accountVariantProviderKindMap[a.variant];
+                const token = tokens.find((t) => t.sub === a.id && t.kind === kind);
+                // console.log('Account', a.id, 'Token', token && token.userId);
+                return (
+                    token && {
+                        updateOne: {
+                            filter: { _id: a.id },
+                            update: {
+                                $set: {
+                                    // Set account.providerUserId to token.userId
+                                    providerUserId: token.userId,
+                                },
+                            },
+                        },
+                    }
+                );
+            })
+            .filter((op) => !!op);
+        console.log('Operations:', operations.length);
+        await Account.bulkWrite(operations);
+        console.log('Finished chunk:', i);
     }
 }
 
