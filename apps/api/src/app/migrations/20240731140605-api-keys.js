10da34ec35169fee5c675a50ff1d500bd0a71187
Peter <peter@thx.network>
2024-08-01 13:49:59 +0200

    Disable maintenance

--- apps/api/src/app/migrations/20240731140605-api-keys.js
+++ apps/api/src/app/migrations/20240731140605-api-keys.js
@@ -18,7 +18,7 @@ module.exports = {
             };
         });
 
-        clientColl.bulkWrite(operations);
+        await clientColl.bulkWrite(operations);
     },
     async down(db, client) {
         //
