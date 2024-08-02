13c3f484dde086b1e3f358103ba7558b5430b14d
Peter <peter@thx.network>
2024-08-02 13:17:37 +0200

    List invoices

--- apps/api/scripts/src/invoices.ts
+++ apps/api/scripts/src/invoices.ts
@@ -1,11 +1,10 @@
 import InvoiceService from '@thxnetwork/api/services/InvoiceService';
-import { startOfMonth, endOfMonth, addHours, subMonths, addMonths } from 'date-fns';
+import { startOfMonth, endOfMonth, addHours, subMonths } from 'date-fns';
 
 export default async function main() {
-    const startDate = subMonths(new Date(), 2);
-    const endDate = addMonths(startDate, 1);
+    const startDate = subMonths(new Date(), 4);
     const invoicePeriodstartDate = startOfMonth(startDate);
-    const invoicePeriodEndDate = endOfMonth(endDate);
+    const invoicePeriodEndDate = endOfMonth(startDate);
 
     // Account for UTC + 2 timezone offset
     const offset = 2;
--- apps/api/src/app/controllers/account/invoices/list.controller.ts
+++ apps/api/src/app/controllers/account/invoices/list.controller.ts
@@ -1,11 +1,10 @@
 import { Request, Response } from 'express';
-import { Invoice, Pool } from '@thxnetwork/api/models';
+import { Invoice } from '@thxnetwork/api/models';
 
 const validation = [];
 
 const controller = async (req: Request, res: Response) => {
-    const poolIds = await Pool.find({ sub: req.auth.sub });
-    const invoices = await Invoice.find({ poolId: { $in: poolIds } });
+    const invoices = await Invoice.find({ sub: req.auth.sub });
 
     res.json(invoices);
 };
