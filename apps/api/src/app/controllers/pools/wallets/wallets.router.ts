c997df2788725c9ba1395676316a30be4a0e828f
Peter <peter@thx.network>
2024-11-11 12:16:25 +0100

    Force rebuild

--- apps/api/src/app/controllers/pools/wallets/wallets.router.ts
+++ apps/api/src/app/controllers/pools/wallets/wallets.router.ts
@@ -5,7 +5,6 @@ import * as ListWallets from './list.controller';
 import * as CreateWallets from './post.controller';
 
 const router: express.Router = express.Router({ mergeParams: true });
-
 router.get('/', assertRequestInput(ListWallets.validation), ListWallets.controller);
 router.post('/', assertRequestInput(CreateWallets.validation), CreateWallets.controller);
 router.delete('/:walletId', assertRequestInput(RemoveWallets.validation), RemoveWallets.controller);
