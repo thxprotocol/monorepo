a58dc1859ff322eeed26ffe26f626301e2ed9416
leo-santa <leo@santabrowser.com>
2025-03-11 13:53:35 +0200

    fix:okx wallet 20

--- apps/app/src/components/tabs/BaseTabWalletWalletConnect.vue
+++ apps/app/src/components/tabs/BaseTabWalletWalletConnect.vue
@@ -117,26 +117,24 @@ export default defineComponent({
                     this.address = response.address;
                     this.publicKey = response.publicKey;
                     this.walletStore.account = { address: response.address };
-                    // if (this.isMobile()) {
-                    //     const santaDeepLink = 'santa://';
-                    //     this.$bvModal
-                    //         .msgBoxConfirm(`Wallet added successfully! Click OK to return to Santa app.`, {
-                    //             title: 'Return to Santa App',
-                    //             okTitle: 'Return to App',
-                    //             cancelTitle: 'Stay Here',
-                    //             centered: true,
-                    //             okVariant: 'primary',
-                    //         })
-                    //         .then((confirmed) => {
-                    //             if (confirmed) {
-                    //                 window.location.href = santaDeepLink;
-                    //             } else {
-                    //                 this.$emit('close');
-                    //             }
-                    //         });
-                    // } else {
-                    //     this.$emit('close');
-                    // }
+                    if (this.isMobile()) {
+                        const santaDeepLink = 'santa://';
+                        this.$bvModal
+                            .msgBoxConfirm(`Wallet added successfully! Please return to Santa app.`, {
+                                title: 'Return to Santa App',
+                                okTitle: 'Return to App',
+                                cancelTitle: 'Stay Here',
+                                centered: true,
+                                okVariant: 'primary',
+                            })
+                            .then((confirmed) => {
+                                if (confirmed) {
+                                    // window.location.href = santaDeepLink;
+                                } else {
+                                    this.$emit('close');
+                                }
+                            });
+                    }
                 } catch (error) {
                     if (error.status === 'Rejected') {
                         this.error = 'Wallet connect is rejected. Please check your wallet.';
