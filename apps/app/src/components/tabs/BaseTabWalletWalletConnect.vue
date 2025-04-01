79a2dfb355f56adc00e76e39a65e25cdcb4d3f77
leo-santa <leo@santabrowser.com>
2025-04-02 00:38:58 +0300

    wallet fix

--- apps/app/src/components/tabs/BaseTabWalletWalletConnect.vue
+++ apps/app/src/components/tabs/BaseTabWalletWalletConnect.vue
@@ -136,7 +136,9 @@ export default defineComponent({
                         this.walletStore.account = { address: response.args.address };
                     } catch (error) {
                         if (error.status === 'Rejected') {
-                            window.open('santa://wallet');
+                            // window.open('santa://wallet');
+                            alert(error);
+                            alert(JSON.parse(window.santaAptos));
                         } else {
                             console.log(error);
                         }
