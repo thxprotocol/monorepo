711bacee78c990d979e5b5347d6613a829b316af
devarogundade <devarogundade@gmail.com>
2025-08-13 16:10:34 +0100

    fix

--- apps/app/src/utils/snowplowTracker.ts
+++ apps/app/src/utils/snowplowTracker.ts
@@ -1,56 +1,55 @@
 import {
-  newTracker,
-  trackPageView,
-  BrowserTracker,
-  setUserId,
-  addGlobalContexts,
-  trackSelfDescribingEvent,
-  enableActivityTracking
+    newTracker,
+    trackPageView,
+    BrowserTracker,
+    setUserId,
+    addGlobalContexts,
+    trackSelfDescribingEvent,
+    enableActivityTracking,
 } from '@snowplow/browser-tracker';
 
 let tracker: BrowserTracker | null | undefined;
 
 const initializeTracker = (endpoint: string, clid: string) => {
-  tracker = newTracker('rewards', endpoint, { appId: 'rewards' });
+    tracker = newTracker('rewards', endpoint, { appId: 'rewards' });
 
-  enableActivityTracking({
-    heartbeatDelay: 30,
-    activityTimeout: 10,
-  });
+    enableActivityTracking({
+        heartbeatDelay: 30,
+        minimumVisitLength: 10,
+    });
 
-  addGlobalContexts([
-    {
-      schema: 'iglu:com.santabrowser/user_context/jsonschema/1-0-0',
-      data: {
-        clid,
-      },
-    },
-  ]);
+    addGlobalContexts([
+        {
+            schema: 'iglu:com.santabrowser/user_context/jsonschema/1-0-0',
+            data: {
+                clid,
+            },
+        },
+    ]);
 
-  setUserId(clid);
+    setUserId(clid);
 };
 
 const useTrackPageview = () => {
-  if (tracker) {
-    trackPageView();
-  }
+    if (tracker) {
+        trackPageView();
+    }
 };
 
 const useTrackSelfDescribingEvent = (clid: string, eventType: string) => {
-  if (tracker) {
-    trackSelfDescribingEvent({
-      event: {
-        schema: 'iglu:com.santabrowser/user_context/jsonschema/1-0-0',
-        data: {
-          clid,
-          eventType,
-        },
-      },
-    });
-  }
+    if (tracker) {
+        trackSelfDescribingEvent({
+            event: {
+                schema: 'iglu:com.santabrowser/user_context/jsonschema/1-0-0',
+                data: {
+                    clid,
+                    eventType,
+                },
+            },
+        });
+    }
 };
 
 const isTrackerInitialized = () => (tracker ? true : false);
 
 export { tracker, initializeTracker, useTrackPageview, isTrackerInitialized, useTrackSelfDescribingEvent };
-
