49e60b3af6e90f6aa672a3d9faa748eed2749bff
Gowtham S <gowtham@santabrowser.com>
2025-09-23 19:07:31 +0530

    Update leaderboard to 100

--- apps/api/src/app/controllers/leaderboards/get.controller.ts
+++ apps/api/src/app/controllers/leaderboards/get.controller.ts
@@ -16,7 +16,7 @@ const controller = async (req: Request, res: Response) => {
 
     const options = { startDate, endDate };
     const leaderboard = await PoolService.getLeaderboardFromCache(pool, options);
-    const result = await PoolService.getLeaderboardTop(leaderboard, 10);
+    const result = await PoolService.getLeaderboardTop(leaderboard, 100);
 
     res.json(result);
 };
